109980d7bdbf76786d2984b27ca5f9a572e9f1e6
davidshtian <david881223@outlook.com>
2022-08-12 15:52:10 +0800

    Update spark-sql-delta-2-simple.py

--- spark-sql-delta-2-simple.py
+++ spark-sql-delta-2-simple.py
@@ -17,18 +17,18 @@ spark = (
     )
     .enableHiveSupport()
     .getOrCreate()
-)
-
-# generate random output path for testing
-if output_path.endswith("/"):
-    url = output_path + str(uuid.uuid4())
-else:
-    url = output_path + "/" + str(uuid.uuid4())                            
+)                         
 
 if output_path:
+    # generate random output path for testing
+    if output_path.endswith("/"):
+        url = output_path + str(uuid.uuid4())
+    else:
+        url = output_path + "/" + str(uuid.uuid4())   
+    
     # creates a Delta table and outputs to target S3 bucket
     spark.range(5).write.format("delta").save(url)
-    print("Writing range data to S3 output: " + output_path)
+    print("Writing range data to S3 output: " + url + ".")
     
     # reads a Delta table and outputs to target S3 bucket (shown in logs)
     spark.read.format("delta").load(url).show()
